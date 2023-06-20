6b1858f074cab5f4db3ad0148605a8e24732db48
Firzenax <louis.20160437@gmail.com>
2023-06-20 16:43:30 +0200

    finished project

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import AnimatedLogo from "../assets/images/logo-animated.gif";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={AnimatedLogo} alt="Apple loader" className="logo" />
+    </div>
+  );
+};
+export default Loader;
